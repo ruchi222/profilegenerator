c1759713a996cc09c5947c4ccfec8b96b589b20d
Ruchika Gosain <58868182+ruchi222@users.noreply.github.com>
2021-07-27 18:26:15 -0400

    Create survey.js

---
+++ survey.js
@@ -0,0 +1,30 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What is your name? ', (answer) => {
+
+rl.question('What is an activity you like doing? ', (answer) => {
+
+rl.question('What do you listen to while doing that? ', (answer) => {
+
+rl.question('Which meal is your favourite (eg: dinner, brunch, etc.) ', (answer) => {
+
+rl.question('What is your favourite thing to eat for that meal?', (answer) => {
+
+rl.question('Which sport is your absolute favourite?', (answer) => {
+
+rl.question('What is your superpower? In a few words, tell us what you are amazing at! ', (answer) => {
+    
+    rl.close();
+    
+});
+});
+});
+});
+});
+});
+});
